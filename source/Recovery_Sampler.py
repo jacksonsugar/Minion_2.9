7b9669e593fa2a26e502a4110bbda346dc3589bf
Jackson Sugar <33731112+jacksonsugar@users.noreply.github.com>
2020-11-12 16:21:49 -0500

    Update Recovery_Sampler.py

--- source/Recovery_Sampler.py
+++ source/Recovery_Sampler.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
             os.system('sudo python /home/pi/Documents/Minion_scripts/ACC_100Hz_IF.py &')
 
         # Spew readings
-        while(NumSamples <= TotalSamples or Pres_ini >= 1500):
+        while(NumSamples <= TotalSamples and Pres_ini >= 1500):
 
             if sensor.read():
                 print("P: %0.1f mbar  %0.3f atm\tT: %0.2f C") % (
